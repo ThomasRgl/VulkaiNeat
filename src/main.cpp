5a2b44c20c33d064e5ce53dee8fc22154694a3c7
Thukisdo <mathys.jam@gmail.com>
2021-03-21 11:22:21 +0100

    Added snake and neural network directories

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,12 @@ using namespace util;
 
 
 int main() {
-
-
     LoggerConfig config;
     config.log_to_file = false;
     config.use_color = true;
     config.ts_type = time::timestamp_t::Partial;
 
     Logger::singleton().config(config);
-
-
     Logger::log(StringLog("Message"));
 
     return 0;
